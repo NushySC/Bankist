84d7722130d661e17a4b16679ae848d7fe713c71
Anuska Sampedro <asampedro@vectoritcgroup.com>
2021-03-08 21:14:57 +0100

    Chaining methods

--- script.js
+++ script.js
@@ -80,19 +80,155 @@ const displayMovements = (movements) => {
 };
 displayMovements(account1.movements);
 
+//Displays Balance
+
+const calcDisplayBalance = (movements) => {
+	const incomes = movements
+		.filter((mov) => mov > 0)
+		.reduce((acc, mov) => acc + mov, 0);
+	labelSumIn.textContent = `${incomes}€`;
+};
+calcDisplayBalance(account1.movements);
+
+//Displays Charges
+
+const calcDisplayCharges = (movements) => {
+	const wastedMoney = movements
+		.filter((mov) => mov < 0)
+		.reduce((acc, mov) => acc + mov, 0);
+	labelSumOut.textContent = `${wastedMoney}€`;
+};
+calcDisplayCharges(account1.movements);
+
+//Displays Interest
+const calcDisplayInterest = (movements) => {
+	const interest = movements
+		.filter((mov) => mov > 0)
+		.map((deposit) => (deposit * 1.2) / 100)
+		.filter((int) => {
+			return int > 1;
+		})
+		.reduce((acc, int) => acc + int, 0);
+	labelSumInterest.textContent = `${interest}`;
+};
+calcDisplayInterest(account1.movements);
+
 //Currency conversion
 const eurToUsd = 1.2;
-const movsUsd = movements.map((mov) => {
+const movementsUsd = movements.map((mov) => {
 	return mov * eurToUsd;
 });
 
-console.log(movements);
-console.log(movsUsd);
+const totalInUSD = movements
+	.filter((mov) => mov > 0)
+	// .map((mov) => mov * eurToUsd)
+	.map((mov, arr, i) => {
+		console.log(arr);
+		return mov * eurToUsd;
+	})
+
+	.reduce((acc, mov) => acc + mov, 0);
+
+console.log(totalInUSD);
+console.log(`the total in USD IS ${totalInUSD}`);
+
+const movementsDesriptions = movements.map((mov, i) => {
+	return `Movement ${i + 1}: You ${
+		mov > 0 ? 'deposited' : 'withdrew'
+	} ${Math.abs(mov)}`;
+	// if (mov > 0) {
+	// 	return `Movement ${i + 1}: You deposited ${mov}`;
+	// } else {
+	// 	return `Movement ${i + 1}: You withdrew ${mov}`;
+	// }
+});
+console.log(movementsDesriptions);
 
 const currencies = new Map([
-	['USD', 'United States dollar'],
+	['USD', 'United States Dollar'],
 	['EUR', 'Euro'],
 	['GBP', 'Pound sterling'],
 ]);
 
-/////////////////////////////////////////////////
+const calcPrintBalance = (movements) => {
+	const balance = movements.reduce((accu, mov) => {
+		return accu + mov;
+	}, 0);
+	labelBalance.textContent = `${balance} €`;
+};
+calcPrintBalance(movements);
+
+const max = movements.reduce((acc, mov) => {
+	if (acc > mov) {
+		return acc;
+	} else {
+		return mov;
+	}
+}, movements[0]);
+console.log(max);
+
+// Generate Usernames
+// const user = account1.owner;
+
+// const createUsername = (user) => {};
+// const userName = user
+// 	.toLowerCase()
+// 	.split(' ')
+// 	.map((name) => {
+// 		return name[0];
+// 	})
+// 	.join('');
+// console.log(userName);
+
+const createUsername = (accs) => {
+	accs.forEach((acc) => {
+		acc.username = acc.owner
+			.toLowerCase()
+			.split(' ')
+			.map((name) => {
+				return name[0];
+			})
+			.join('');
+	});
+
+	// const userName = user
+	// 	.toLowerCase()
+	// 	.split(' ')
+	// 	.map((name) => {
+	// 		return name[0];
+	// 	})
+	// 	.join('');
+	// return userName;
+};
+// console.log(createUsername(user));
+
+createUsername(accounts);
+console.log(accounts);
+
+//Filter
+const deposits = movements.filter((mov) => {
+	return mov > 0;
+});
+
+console.log(deposits);
+
+const depositsFor = [];
+
+for (const mov of movements)
+	if (mov > 0) {
+		depositsFor.push(mov);
+		console.log(depositsFor);
+	}
+
+const withdrawals = movements.filter((mov) => {
+	return mov < 0;
+});
+
+console.log(withdrawals);
+
+//Reduce
+const blnce = movements.reduce((accu, curnt, i, arr) => {
+	console.log(`Iteration #${i}: ${accu}`);
+	return accu + curnt;
+}, 0);
+console.log(blnce);
